464e7a1e4bfefc188ae85fdffb8616d9e46a2e8c
Hesbon Angwenyi <hesbonmanyinsa96@gmail.com>
2025-11-14 12:42:16 +0300

    done

--- src/Database.php
+++ src/Database.php
@@ -1,17 +1,19 @@
-<?php
-namespace App;
-
-use PDO;
-
-class Database {
-    private static $pdo;
-
-    public static function get(): PDO {
-        if (!self::$pdo) {
-            $path = __DIR__ . '/../data/database.sqlite';
-            self::$pdo = new PDO('sqlite:' . $path);
-            self::$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        }
-        return self::$pdo;
-    }
-}
+hesbonangwenyi@hesbonangwenyi-HP-ProBook-430-G5:~/Downloads/php_simple_crud$ php -S localhost:8000 -t public
+[Fri Nov 14 12:40:25 2025] PHP 8.2.29 Development Server (http://localhost:8000) started
+[Fri Nov 14 12:40:29 2025] [::1]:55192 Accepted
+[Fri Nov 14 12:40:29 2025] [::1]:55192 [200]: GET /
+[Fri Nov 14 12:40:29 2025] [::1]:55192 Closing
+[Fri Nov 14 12:40:29 2025] [::1]:55206 Accepted
+[Fri Nov 14 12:40:29 2025] [::1]:55208 Accepted
+[Fri Nov 14 12:40:32 2025] [::1]:55206 [200]: GET /create.php
+[Fri Nov 14 12:40:32 2025] [::1]:55206 Closing
+[Fri Nov 14 12:40:33 2025] [::1]:56178 Accepted
+[Fri Nov 14 12:41:01 2025] [::1]:55208 [302]: POST /create.php
+[Fri Nov 14 12:41:01 2025] [::1]:55208 Closing
+[Fri Nov 14 12:41:01 2025] [::1]:56178 [200]: GET /index.php
+[Fri Nov 14 12:41:01 2025] [::1]:56178 Closing
+[Fri Nov 14 12:41:05 2025] [::1]:50196 Accepted
+[Fri Nov 14 12:41:05 2025] [::1]:50196 [200]: GET /create.php
+[Fri Nov 14 12:41:05 2025] [::1]:50196 Closing
+[Fri Nov 14 12:41:05 2025] [::1]:50206 Accepted
+[Fri Nov 14 12:41:05 2025] [::1]:50218 Accepted
