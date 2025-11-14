7d82ba949f47f337ee224fe566f5ba14e9a158d2
Hesbon Angwenyi <hesbonmanyinsa96@gmail.com>
2025-11-14 12:36:00 +0300

    updated

--- src/Database.php
+++ src/Database.php
@@ -1,3 +1,17 @@
-This page isn’t working
-localhost is currently unable to handle this request.
-HTTP ERROR 500
+<?php
+namespace App;
+
+use PDO;
+
+class Database {
+    private static $pdo;
+
+    public static function get(): PDO {
+        if (!self::$pdo) {
+            $path = __DIR__ . '/../data/database.sqlite';
+            self::$pdo = new PDO('sqlite:' . $path);
+            self::$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        }
+        return self::$pdo;
+    }
+}
